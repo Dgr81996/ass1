edbcc400ee9d0b78eb133294d461114b6cc1fb25
Deepak Choudhary <97680719+Dgr81996@users.noreply.github.com>
2022-05-09 16:55:09 +0530

    Add files via upload

---
+++ 6a/src/App.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+function App() {
+  return (
+    <div>
+     <h1>Mobile Operating System</h1>
+     <p>*  Anderoid</p>
+     <p>*  Blackberry</p>
+     <p>* iphone</p>
+     <p>*  Windeows</p>
+
+     <h1>Mobile Manufacturer</h1>
+     <p>*  Samsung</p>
+     <p>* Htc</p>
+     <p>* Micromax</p>
+     <p>*  Apple</p>
+
+
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
